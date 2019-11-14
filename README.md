fa9e8006c4ab5cc3281be26893c0734839a49ad8
t-goto <t-goto@infocity.co.jp>
2019-11-14 15:46:54 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# RichFlyer Event SDK for Swift
+
+本SDKは、[RichFlyer SDK](https://github.com/INFOCITY/richflyer-sdk-swift)と共に利用します。
+現在、単体では利用できません。
+
+利用方法は[SDKご利用ガイド](https://richflyer.net/sdk/manual/ios/)をご覧ください。
---
+++ RichFlyerEvent.framework/Headers/RichFlyerEvent.h
@@ -0,0 +1,18 @@
+//
+//  RichFlyerEvent.h
+//  RichFlyerEvent
+//
+//  Copyright © 2019年 INFOCITY,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RichFlyerEvent.
+FOUNDATION_EXPORT double RichFlyerEventVersionNumber;
+
+//! Project version string for RichFlyerEvent.
+FOUNDATION_EXPORT const unsigned char RichFlyerEventVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichFlyerEvent/PublicHeader.h>
+
+
